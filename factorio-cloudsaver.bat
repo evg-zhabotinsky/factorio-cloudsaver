068b9d47aebf5bc7614dd80021425d7701b24d0c
Evgeny Zhabotinskiy <evg-zhabotinsky@yandex.ru>
2025-01-26 17:46:36 +0300

    F* this, replace the huge .ps1 bootstrap with a tiny .bat
    That ps1 may have been cool and universal and all that, but it got longer than the python script it was supposed to launch!

--- factorio-cloudsaver.bat
+++ factorio-cloudsaver.bat
@@ -1,1 +1,5 @@
-@powershell -executionpolicy Bypass -File "%~dpn0.ps1" %*
+@echo off
+if not exist "%~dp0\python" powershell -Command "$v = '3.6.8'; $p = '%~dp0\python'; $z = '$p-$v.zip';" ^
+        "Invoke-WebRequest -Uri https://www.python.org/ftp/python/$v/python-$v-embed-amd64.zip -OutFile $z;" ^
+        "Expand-Archive -Path $z -DestinationPath $p; Remove-Item $z"
+"%~dp0\python\python.exe" "%~dpn0.py" %*
